96652c1567450e7096d1aa915fe90bde02216b84
YvZhang <zytuatua@163.com>
2024-12-17 09:18:48 +0800

    init:init project structure

---
+++ enginee/src/test/java/com/zy/enginee/EngineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.enginee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
